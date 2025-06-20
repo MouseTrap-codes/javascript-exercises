e154a277785e1796ee81f74f8b591e4fd6822eb1
MouseTrap-codes <mousetrapcodes@gmail.com>
2025-06-20 00:38:31 -0500

    Finish 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...toRemove) {
+    for (let element of toRemove) {
+        while (arr.indexOf(element) !== -1) {
+            arr.splice(arr.indexOf(element), 1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
