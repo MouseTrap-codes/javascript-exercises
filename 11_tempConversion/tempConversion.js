8022a98ddb58861d33a1dc7d5fefc60394563d08
MouseTrap-codes <mousetrapcodes@gmail.com>
2025-06-20 01:04:42 -0500

    Finish 11 tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  temp = parseFloat(((temp - 32) * 5 / 9).toFixed(1));
+  return temp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  temp = parseFloat(((temp * 9 / 5) + 32).toFixed(1));
+  return temp;
 };
 
 // Do not edit below this line
