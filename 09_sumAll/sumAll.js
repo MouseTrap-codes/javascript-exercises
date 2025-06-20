bba3166de8a9cbfd8d7a44df10f8ec544e405ff2
MouseTrap-codes <mousetrapcodes@gmail.com>
2025-06-20 00:50:13 -0500

    Finish 09 sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (typeof a !== "number" || typeof b !== "number") {
+        return "ERROR";
+    }
 
+    if (a < 0 || b < 0) {
+        return "ERROR";
+    }
+
+    if (Math.floor(a) !== a || Math.floor(b) !== b) {
+        return "ERROR";
+    }
+
+    let sum = 0;
+    let start = Math.min(a, b);
+    let end = Math.max(a, b);
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
